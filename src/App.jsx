2c128999629a24bb590b572d1c4fb20ac0bddc3e
David Noguera <david.noguera@techmahindra.com>
2023-11-24 11:08:37 +1100

    modify home page

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>Learn & Master GitHub Actions - runner</h1>
+        <h1>Learn & Master GitHub Actions - with AWS!</h1>
       </header>
       <MainContent />
     </>
